aaffa9f472ba01e4f1bbfb154127e10b1f3b7fd4
Gabriel Moreno <gantoreno@gmail.com>
2020-12-18 09:56:18 -0400

    Remove unused useState import on App

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
