1749b552fd6b345fbd1c0a4e822e54c143eb8b90
Gabriel Moreno <gantoreno@gmail.com>
2020-12-18 11:10:09 -0400

    Extract Card & Project to ProjectsView

--- src/views/projects/ProjectsView.jsx
+++ src/views/projects/ProjectsView.jsx
@@ -6,20 +6,95 @@ import {
   FadedHeader,
   Wrapper,
   NavigationButtons,
-  Card,
-  Project,
+  Button,
 } from '../../shared';
 
 import content from './content.json';
+import languages from '../../extras/languages.json';
 
 import './ProjectsView.scss';
+
+const CommercialProjectOverview = ({
+  pictureUrl,
+  title,
+  behanceLink,
+  language,
+}) => {
+  return (
+    <div className="mb-6 lg:flex content-center flex-wrap bg-grey rounded shadow-lg">
+      <div className="lg:flex lg:w-1/3">
+        <div className="lg:flex-1 rounded overflow-hidden">
+          <img src={pictureUrl} alt={title} />
+        </div>
+      </div>
+      <div className="lg:flex lg:w-2/3">
+        <div className="lg:flex-1">
+          <div className="mt-6 lg:mt-0 lg:ml-6 text-left flex flex-col justify-center items-start h-full">
+            <h1 className="mb-6 font-bold text-xl sm:text-2xl text-left text-white">
+              {title}
+            </h1>
+            <a href={behanceLink} target="_blank" rel="noreferrer">
+              <Button
+                icon={'fab fa-behance ml-3'}
+                text={
+                  language === languages.EN
+                    ? 'View on Bēhance'
+                    : 'Ver en Bēhance'
+                }
+                hoverBackground="red-400"
+                opaque
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const GitHubProjectCard = ({
+  emoji,
+  title,
+  description,
+  link,
+  className,
+  language,
+}) => {
+  return (
+    <div
+      className={`bg-gray-900 rounded border-2 border-gray-800 text-center ${className}`}
+    >
+      <div className="p-6 border-b-2 border-gray-800">
+        <h3 className="text-6xl mb-6">{emoji}</h3>
+        <h3 className="text-xl sm:text-2xl font-bold text-white">{title}</h3>
+      </div>
+      <div className="p-6 border-b-2 border-gray-800">
+        <p className=" text-left text-xs sm:text-base leading-loose text-gray-400">
+          {description}
+        </p>
+      </div>
+      <div className="p-6">
+        <a href={link} target="_blank" rel="noreferrer">
+          <Button
+            icon={'fab fa-github'}
+            text={
+              language === languages.EN ? `View on GitHub` : `Ver en GitHub`
+            }
+            hoverBackground={'red-400'}
+            opaque
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 const CommercialProjectsList = ({ body, language }) => {
   return (
     <div className="mb-12">
       <p className="mb-6">{parse(body[language])}</p>
       {body.projects.map(project => (
-        <Project
+        <CommercialProjectOverview
           id={project.id}
           key={project.id}
           title={project.title[language]}
@@ -41,7 +116,7 @@ const PersonalProjectsGrid = ({ body, language }) => {
           {body.projects.map(
             (project, index) =>
               index % 2 === 0 && (
-                <Card
+                <GitHubProjectCard
                   id={project.id}
                   key={project.id}
                   emoji={project.emoji}
@@ -58,7 +133,7 @@ const PersonalProjectsGrid = ({ body, language }) => {
           {body.projects.map(
             (project, index) =>
               index % 2 !== 0 && (
-                <Card
+                <GitHubProjectCard
                   id={project.id}
                   key={project.id}
                   emoji={project.emoji}
