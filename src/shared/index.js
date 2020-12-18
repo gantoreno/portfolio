e24295c9ed8102e2ff252e731588031008e5d268
Gabriel Moreno <gantoreno@gmail.com>
2020-12-18 11:13:52 -0400

    Remove Card & Project imports from shared

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,9 +1,7 @@
 export { default as Button } from './button/Button';
-export { default as Card } from './card/Card';
 export { default as FadedHeader } from './faded-header/FadedHeader';
 export { default as Entry } from './entry/Entry';
 export { default as Section } from './section/Section';
-export { default as Project } from './project/Project';
 export { default as Wrapper } from './wrapper/Wrapper';
 export { default as NavigationButtons } from './navigation-buttons/NavigationButtons';
 export { default as SocialMediaButtons } from './social-media-buttons/SocialMediaButtons';
