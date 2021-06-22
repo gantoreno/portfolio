86843597e4d83b1a8e71de396381396a5688286c
Gabriel Moreno <gantoreno@gmail.com>
2021-06-22 13:03:19 -0400

    Hide scrollbars on `Wrapper.jsx`

--- src/shared/wrapper/Wrapper.jsx
+++ src/shared/wrapper/Wrapper.jsx
@@ -3,7 +3,7 @@ import './Wrapper.scss';
 const Wrapper = ({ children, className }) => {
   return (
     <div
-      className={`h-screen flex flex-col overflow-y-scroll relative ${className}`}
+      className={`wrapper h-screen flex flex-col overflow-y-scroll relative ${className}`}
     >
       {children}
     </div>
