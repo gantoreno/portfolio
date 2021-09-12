eb51f9ba34ccab037291ee88aad8d0d386c53edb
Gabriel Moreno <gantoreno@gmail.com>
2021-09-12 14:56:05 -0400

    Update color palette on `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,30 @@ module.exports = {
       colors: {
         gray: {
           ...colors.gray,
+          600: '#404040',
           700: '#171717',
           800: '#101010',
           900: '#0d0d0d',
+        },
+        yellow: {
+          ...colors.yellow,
+          400: '#bfa86b',
+        },
+        green: {
+          ...colors.green,
+          400: '#98b386',
+        },
+        purple: {
+          ...colors.purple,
+          400: '#999dd4',
+        },
+        red: {
+          ...colors.red,
+          400: '#ac616a',
+        },
+        white: {
+          ...colors.white,
+          DEFAULT: '#d0d0d0',
         },
       },
     },
