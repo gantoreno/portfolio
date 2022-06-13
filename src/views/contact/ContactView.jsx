71ed27bc7c177298dae882d8b3cd757b4197bffa
Gabriel Moreno <gantoreno@gmail.com>
2022-06-13 09:51:32 -0400

    Remove contact form

--- src/views/contact/ContactView.jsx
+++ src/views/contact/ContactView.jsx
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet';
 
 import {
   Entry,
-  Button,
   Section,
   Wrapper,
   FadedHeader,
@@ -13,7 +12,6 @@ import {
 import { useLanguage } from '../../hooks';
 
 import content from './content.json';
-import languages from '../../extras/languages';
 
 import './ContactView.scss';
 
@@ -24,65 +22,6 @@ const SocialMedia = ({ body, language }) => {
         {parse(body[language])}
       </p>
       <SocialMediaButtons />
-    </div>
-  );
-};
-
-const ContactForm = ({ body, language }) => {
-  return (
-    <div className="mb-12">
-      <p className="mb-6 text-xs leading-loose text-left text-gray-600 sm:text-base">
-        {parse(body[language])}
-      </p>
-      <div className="mb-6 text-xs text-gray-600 sm:text-base">
-        <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 lg:mb-6">
-          <input
-            type="text"
-            placeholder={language === languages.EN ? `First name` : `Nombre`}
-            className="p-3 mb-6 text-gray-600 bg-transparent border border-gray-700 rounded lg:mr-3 lg:mb-0 focus:text-white focus:border-white"
-            required
-          />
-          <input
-            type="text"
-            placeholder={language === languages.EN ? `Last name` : `Apellido`}
-            className="p-3 mb-6 text-gray-600 bg-transparent border border-gray-700 rounded lg:ml-3 lg:mb-0 focus:text-white focus:border-white"
-            required
-          />
-        </div>
-        <div className="grid grid-cols-1 mb-6">
-          <input
-            type="email"
-            placeholder={
-              language === languages.EN ? `E-mail` : `Correo electrónico`
-            }
-            className="p-3 text-gray-700 bg-transparent border border-gray-700 rounded focus:text-white focus:border-white"
-            required
-          />
-        </div>
-        <div className="grid grid-cols-1 mb-6">
-          <textarea
-            name="message"
-            rows="10"
-            className="p-3 text-gray-600 bg-transparent border border-gray-700 rounded focus:text-white focus:border-white"
-            placeholder={
-              language === languages.EN
-                ? `What would you like to build?`
-                : `¿Qué te gustaría construir?`
-            }
-            resize="vertical"
-            required
-          ></textarea>
-        </div>
-        <div className="text-center">
-          <Button
-            text={'Send'}
-            rightIcon={'fas fa-paper-plane'}
-            hoverBackground={'purple-400'}
-            className={'mx-auto'}
-            opaque
-          />
-        </div>
-      </div>
     </div>
   );
 };
@@ -104,9 +43,6 @@ const ContactView = () => {
           <Entry id={id} key={id} title={parse(entry.title[language])}>
             {entry.body.type === 'contact:media' && (
               <SocialMedia body={entry.body} language={language} />
-            )}
-            {entry.body.type === 'contact:form' && (
-              <ContactForm body={entry.body} language={language} />
             )}
           </Entry>
         ))}
